59e35e339923c634229e444e21a52aea13fad061
Zhicheng Sun <zhch.sun@gmail.com>
2019-06-21 19:35:24 +0800

    two sum succeed

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -10,5 +10,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        map = {}
+        for i in range(len(nums)):
+            if nums[i] not in map:
+                map[target - nums[i]] = i
+            else:
+                return map[nums[i]], i
+        return -1, -1
+
+
+if __name__ == '__main__':
+    s = Solution()        
+    out = s.twoSum([2,7,11,15], 22)
+    print(out)
 
