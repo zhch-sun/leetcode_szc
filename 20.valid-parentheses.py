a552f8b977f88d6d922b8c33ebae710ffaffcbc4
Zhicheng Sun <zhch.sun@gmail.com>
2019-06-22 17:47:42 +0800

    20th

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=20 lang=python
+#
+# [20] Valid Parentheses
+#
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {
+            ')': '(',
+            ']': '[',
+            '}': '{',
+        }
+        for ch in s:
+            if ch in mapping:
+                if not stack or not stack.pop() == mapping[ch]:
+                    return False
+            else:
+                stack.append(ch)
+        return not stack
+
+if __name__ == '__main__':
+    """
+    two pitfalls
+    1. the empty stack for popping
+    2. in the end stack might not be empty
+    """
+    s = Solution()
+    print(s.isValid('(()))'))
+
