09a6fa4ca29cdf9a6caa88c37b99264e505d6f88
Zhicheng Sun <zhch.sun@gmail.com>
2019-06-30 18:20:50 +0800

    136th

---
+++ 136.single-number.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=136 lang=python
+#
+# [136] Single Number
+#
+import operator
+class Solution(object):
+    # def singleNumber(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     res = 0
+    #     for item in nums:
+    #         res = res ^ item
+    #     return res
+        
+    def singleNumber(self, nums):
+        return reduce(operator.xor, nums)
+    
+    # def singleNumber(self, nums):
+    #     return 2* sum(set(nums)) - sum(nums)
+
+
+if __name__ == '__main__':
+    """
+    1. 类似two sum, 用dict记录
+    2. 用亦或 ^ operator.xor (不一样的时候为1, 一样的时候为0)
+    3. ^ 和 reduce
+    4. 数学大法: 求2*set(nums) - sum(nums): 注意这个linear memory.
+    """
+    s = Solution()
+    print(s.singleNumber([4,1,2,1,2]))
