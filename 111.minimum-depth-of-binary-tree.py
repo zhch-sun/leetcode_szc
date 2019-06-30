5fe745a019941f5730ad2b76086a90386df64972
Zhicheng Sun <zhch.sun@gmail.com>
2019-06-30 14:36:52 +0800

    111st with BFS

--- 111.minimum-depth-of-binary-tree.py
+++ 111.minimum-depth-of-binary-tree.py
@@ -4,31 +4,89 @@
 # [111] Minimum Depth of Binary Tree
 #
 # Definition for a binary tree node.
+from collections import deque
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+    
 
 class Solution(object):
+    # def minDepth(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: int
+    #     """
+    #     # This is the DFS version
+    #     if not root:
+    #         return 0
+    #     left = self.minDepth(root.left)
+    #     right = self.minDepth(root.right)
+    #     if left == 0 or right == 0:
+    #         return left + right + 1
+    #     else:
+    #         return min(left, right) + 1
+    
     def minDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
         if not root:
             return 0
-        left = self.minDepth(root.left)
-        right = self.minDepth(root.right)
-        if left == 0 or right == 0:
-            return left + right + 1
-        else:
-            return min(left, right) + 1
+        queue = deque([root])
+        level = 0
+        while queue:
+            size = len(queue)
+            for _ in xrange(size):
+                node = queue.popleft()  # Note this is not pop!!
+                if node.left is None and node.right is None:
+                    # Note this is a leaf!
+                    return level + 1
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            level += 1
+
+def listToTree(input):
+    if not input:
+        return None
+    root = TreeNode(int(input[0]))
+    nodeQueue = [root]  # 最后这个包含所有node
+    front = 0  # to index queue
+    index = 1  # to index list
+    while index < len(input):
+        node = nodeQueue[front]
+        front += 1
+
+        left_num = input[index]
+        if left_num is not None:
+            # 这意味着前面的None会使后面的位置直接跳过
+            node.left = TreeNode(left_num)
+            nodeQueue.append(node.left)
+
+        index += 1
+        if index >= len(input):
+            break
+
+        right_num = input[index]
+        if right_num is not None:
+            node.right = TreeNode(right_num)
+            nodeQueue.append(node.right)
+        index += 1
+    return root
 
 if __name__ == '__main__':
     """
+    BFS更快
     Note - can not directly use min(left, right) + 1  ....
+    Note - 必须是root 到 leaf的距离, 当root有一个分支是none的时候, depth不是1 !
+    leaf的定义是一个没有children的node. 
+    当只有一个root的时候, root本身也是一个leaf
     """
     s = Solution()
+    tree = listToTree([3,9,20,None,None,15,7])
+    tree = listToTree([1,2])
+    tree = listToTree([1,2,3,4,None,None,5])
+    print(s.minDepth(tree))
         
 
---
+++ 112.path-sum.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=112 lang=python
+#
+# [112] Path Sum
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        
+
