472e4fbbd0abc455d3d61bb66e56a36577effd06
Zhicheng Sun <zhch.sun@gmail.com>
2019-07-01 14:14:45 +0800

    172th

---
+++ 172.factorial-trailing-zeroes.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=172 lang=python
+#
+# [172] Factorial Trailing Zeroes
+#
+class Solution(object):
+    # def trailingZeroes(self, n):
+    #     """
+    #     :type n: int
+    #     :rtype: int
+    #     """
+    #     return 0 if n < 5 else n / 5 + self.trailingZeroes(n / 5)
+        
+    def trailingZeroes(self, n):
+        count = 0
+        while n >= 5:
+            n /= 5
+            count += n
+        return count
+
+if __name__ == '__main__':
+    """
+    理解错误啦
+    第一个除以五是判断能被5整除的数:
+    但是25里面有两个5, 所以再除以5看多少个能被25整除, 
+    然后125...
+    """
+    s = Solution()
