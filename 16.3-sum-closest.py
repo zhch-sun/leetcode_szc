4011f8d366533dbcd778f056249f48ddd7391e68
Zhicheng Sun <zhch.sun@gmail.com>
2019-07-03 20:22:32 +0800

    16th finish

--- 16.3-sum-closest.py
+++ 16.3-sum-closest.py
@@ -3,34 +3,29 @@
 #
 # [16] 3Sum Closest
 #
+
 class Solution(object):
-
-
     def threeSumClosest(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        def twoSum(nums, target, left, closest):
-            l = left
+        nums.sort()
+        closest = float('inf')
+        for idx, item in enumerate(nums):
+            l = idx + 1
             r = len(nums) - 1
             while l < r:
-                cur = nums[l] + nums[r]
-                closest = closest if abs(closest) < abs(target - cur) else target - cur
+                cur = item + nums[l] + nums[r] 
+                closest = closest if abs(target-closest) < abs(target-cur) \
+                    else cur
                 if cur < target:
                     l += 1
                 elif cur > target:
                     r -= 1
                 else:
                     return target
-
-        nums.sort()
-        closest = float('inf')
-        for idx, item in enumerate(nums):
-            closest = twoSum(nums, target - item, idx + 1, closest) + item
-            if closest == target:
-                return closest
         return closest
         
 if __name__ == '__main__':
