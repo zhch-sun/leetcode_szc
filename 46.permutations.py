7459973be680a54a02e3cedfb2cb8c05bd6fb8ee
zhicheng <zhch.sun@gmail.com>
2019-07-11 21:37:40 +0800

    add 46th

---
+++ 46.permutations.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=46 lang=python
+#
+# [46] Permutations
+#
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+if __name__ == '__main__':
+    """
+    TODO backtracking是什么鬼？？？
+    确实是个dfs，只不过这里不是binary而是n-ary tree。
+    dfs可以用swap，好处是省掉了memcopy的时间。python的答案都有很多memcopy
+    还有iterative的做法，
+    """
+    s = Solution()
+    
+
