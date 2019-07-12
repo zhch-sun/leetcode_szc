f1cdf56946ca32122344c0fc427171fa7666d3dd
Zhicheng Sun <zhch.sun@gmail.com>
2019-07-12 16:04:36 +0800

    46th 47th analysis

--- 46.permutations.py
+++ 46.permutations.py
@@ -3,19 +3,24 @@
 #
 # [46] Permutations
 #
+import itertools
 class Solution(object):
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        return itertools.permutations
         
 if __name__ == '__main__':
     """
     TODO backtracking是什么鬼？？？
     确实是个dfs，只不过这里不是binary而是n-ary tree。
     dfs可以用swap，好处是省掉了memcopy的时间。python的答案都有很多memcopy
-    还有iterative的做法，
+    还有iterative的做法，基础是插入法, 一个一个数字插入. 1只有一个位置, 2有两个, 3有 2*3=6个 4有6*4=64个
+    https://leetcode.com/problems/permutations/discuss/18237/My-AC-simple-iterative-javapython-solution
+    上面的做法要求copy array, 很慢. 下面用swap解决了这个问题. 生长过程是9*8*7*6*5... 不知道怎么证明, 不搞. 
+    https://leetcode.com/problems/permutations/discuss/18378/Simple-python-code-without-recursion
     """
     s = Solution()
     
---
+++ 47.permutations-ii.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=47 lang=python
+#
+# [47] Permutations II
+#
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+if __name__ == '__main__':
+    """
+    就是第一题的dfs的swap, 如果两个数相等就不swap. 
+    对于插入法: To handle duplication, just avoid inserting a number AFTER any of its duplicates.
+    这个的理解方法是对称性. 
+    """
+    s = Solution()
