36f96b0f6ee0d0b8b1831ad2e743970715509847
Zhicheng Sun <zhch.sun@gmail.com>
2019-07-13 21:09:34 +0800

    29th

--- 29.divide-two-integers.py
+++ 29.divide-two-integers.py
@@ -10,14 +10,36 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
+        positive = (dividend < 0) == (divisor < 0)  # 必须括号
+        a, b = abs(dividend), abs(divisor)
+        res = 0
+        while a >= b:  # >=
+            temp, i = b, 1
+            while a >= temp:
+                a -= temp
+                res += i  # 顺序
+                temp <<= 1
+                i <<= 1
+
+        if not positive:
+            return max(-2147483648, -res)  # 2 ** 31
+        else:
+            return min(2147483647, res) 
 
 if __name__ == '__main__':
     """
-    只有一种答案。
-    需要处理最大值以及正负。不能直接取abs，因为负数最大值会溢出。
+    只有一种答案。用减法来做除法。通过右移来实现指数级增加查找范围。
+    corner case: 需要处理最大值以及正负。不能直接取abs，因为负数最大值会溢出。
+    但是毕竟是python, 所以我直接用long了? 
     另外 -div = 0 - div
-    用减法来做除法。通过右移来实现指数级增加查找范围。
     """
     s = Solution()
-       
+    print(s.divide(4, 3))
+    print(s.divide(5, 3))
+    print(s.divide(6, 3))
+    print(s.divide(-7, 3))
+    print(s.divide(8, 3))
+    print(s.divide(10, 3))
+    print(s.divide(-37, 3))
+    print(s.divide(-2147483648, -1))
 
