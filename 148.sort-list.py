3ce2be07d3b9f232a3edef4f32371df2207edf1e
Zhicheng Sun <zhch.sun@gmail.com>
2019-07-23 23:30:39 +0800

    93th

---
+++ 148.sort-list.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=148 lang=python
+#
+# [148] Sort List
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+if __name__ == '__main__':
+    """
+    TODO 这题必须要写bottom-up的merge sort. 好坑爹呀.. 以后再写吧.
+    """
+    s = Solution()
---
+++ 93.restore-ip-addresses.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=93 lang=python
+#
+# [93] Restore IP Addresses
+#
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        def isValid(s):
+            if len(s) > 3 or len(s) == 0 or int(s) > 255 or (s[0] == '0' and len(s) > 1): # 注意括号里
+                return False
+            return True
+                
+        if len(s) < 4:
+            return []
+        res = []
+        for i in range(1, 4):
+            for j in range(i + 1, min(i + 1 + 3, len(s) - 1)):
+                for k in range(j + 1, min(j + 1 + 3, len(s))): #注意边界条件
+                    if isValid(s[:i]) and isValid(s[i:j]) and isValid(s[j:k]) and isValid(s[k:]):
+                        res.append(s[:i] + '.' + s[i:j] + '.' + s[j:k] + '.' + s[k:])
+        return res
+
+        
+if __name__ == '__main__':
+    """
+    也可以backtracking. 但还是直接写for循环. 
+    """
+    s = Solution()
+    # print(s.restoreIpAddresses("25525511135"))
+    # print(s.restoreIpAddresses("2012002123"))
+    print(s.restoreIpAddresses("0000"))
