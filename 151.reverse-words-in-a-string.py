18c2d1df75e4084bf389b2ea51c40d697b8527bb
zhch.sun@gmail.com <Zhicheng Sun>
2019-08-05 15:23:39 +0800

    151 2nd

--- 151.reverse-words-in-a-string.py
+++ 151.reverse-words-in-a-string.py
@@ -18,55 +18,46 @@ class Solution(object):
                 lo += 1
                 hi -= 1
 
-        def clean_space(s):
-            i = j = 0
-            while j < len(s) and s[j] == ' ':
-                j += 1
-            while j < len(s):
-                if s[j] != ' ':
-                    s[i] = s[j]
-                    i += 1
-                j += 1
-
-
-            return s, lo, hi
         if not s:
             return s
-        # clean space            
+        # reverse whole string          
         s = list(s)  # convert it to list to make it mutable
-        lo, hi = 0, len(s) - 1
-        while lo <= hi:
-            if s[lo] == ' ':
+        reverse(s, 0, len(s) - 1)
+        
+        insert = 0 # points to the pos to be written
+        lo = 0  # begin of current word, end of current word
+        while lo < len(s):
+            if s[lo] != ' ':
+                if insert != 0:
+                    s[insert] = ' '  # 必须这里加空格，只有找到一个word才能在前面加。
+                    insert += 1
+                hi = lo
+                while hi < len(s) and s[hi] != ' ':  #必须调换顺序。。 先判断len。。
+                    s[insert] = s[hi]
+                    hi += 1  # hi == ' '
+                    insert += 1
+                reverse(s, insert - (hi - lo), insert - 1)
+                lo = hi + 1
+            else:  # Note这里需要else。。否则lo+的就不对了。。
                 lo += 1
-            elif s[hi] == ' ':
-                hi -= 1
-            else:
-                break
-        if lo > hi:
-            return ''
-        # reverse whole string
-        reverse(s, lo, hi)   
-        # reverse each word
-        start = lo
-        for i in range(lo, hi + 1):
-            char = s[i]
-            if char == ' ' or i == hi:
-                reverse(s, start, i - 1)  # Note it is i - 1...
-                start = i + 1
-            
+            # 因为corner case不能在这里加空格。
 
-        ret = ''.join([s[i] for i in range(lo, hi+1)])
-        return ret
+        return ''.join(s[:insert])
 
 
 if __name__ == '__main__':
     """
     解法1: 库函数. 
-    解法2:
+    解法2：
+        先整个reverse，再维护三个pointer，insert，beginword, endword
+        天哪真是太复杂了。。面试的时候还是写解法3吧。。还是要熟练。。
+    解法3: 
         clean space, reverse whole string, and reverse each word.
-        注意还需要去除中间的多余空格.. 不仅是两头的. 
+        注意clean space还需要去除中间的多余空格.. 不仅是两头的. 写起来并不简单。。
+        四个while循环
     """
     s = Solution()
+    print(s.reverseWords("a good   example"))
     print(s.reverseWords("the sky is blue"))
     print(s.reverseWords("  the  sky    is blue  "))
 
