4464941854497a271855e9df5c32b65ee2730d89
zhicheng <zhch.sun@gmail.com>
2019-08-27 20:14:57 +0800

    add 215

---
+++ 215.kth-largest-element-in-an-array.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=215 lang=python
+#
+# [215] Kth Largest Element in an Array
+#
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+if __name__ == '__main__':
+    """
+    可以用priority queue和quick selection.
+    """
+    s = Solution()
+    
+
